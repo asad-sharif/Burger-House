6e2dbab89055a720e49448d70f552852c3f3d82b
asad-sharif <asadsharif1270@gmail.com>
2023-10-14 12:14:39 +0500

    new commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,13 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+window.onscroll = function () {
+    let nav = document.querySelector('.second-navbar');
+    if (window.scrollY > 100) {
+        nav.classList.add('sticky');
+    }
+    else {
+        nav.classList.remove('sticky');
+    }
+}
